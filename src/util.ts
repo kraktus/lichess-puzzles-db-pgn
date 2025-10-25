c39d97eff3d256266ba10f0926f1b0c9d48f2bd1
kraktus <kraktus@users.noreply.github.com>
2025-10-25 16:15:48 +0200

    code-golf

--- src/util.ts
+++ src/util.ts
@@ -13,9 +13,5 @@ export function ifAny<T>(l: T[], f: (t: T) => boolean): boolean {
 // Toggle Element of the set, if it's there, remove it, otheriwe, add it
 // TODO check if already possible to do (offline)
 export function toggleElm<T>(s: Set<T>, elm: T) {
-  if (s.has(elm)) {
-    s.delete(elm);
-  } else {
-    s.add(elm);
-  }
+  s.has(elm) ? s.delete(elm) : s.add(elm);
 }
