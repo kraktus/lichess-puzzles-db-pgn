1c970330845292fcd6ae6c12828fe905b11999f0
kraktus <kraktus@users.noreply.github.com>
2025-10-25 17:58:25 +0200

    install `@bokuweb/zstd-wasm` to decompress zstd puzzle csv

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss()
-  ],
+  plugins: [tailwindcss()],
+  optimizeDeps: {
+    exclude: ["@bokuweb/zstd-wasm"],
+    esbuildOptions: {
+      target: "es2020",
+    },
+  },
 });
