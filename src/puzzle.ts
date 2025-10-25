e28ffdc864c3e8850d7f21e2102c3165540ba96e
kraktus <kraktus@users.noreply.github.com>
2025-10-25 23:01:34 +0200

    make IndexedDb code compiles

--- src/puzzle.ts
+++ src/puzzle.ts
@@ -1,13 +1,46 @@
 const prefix = "lipuzzles-csv";
+const objectStore = "db";
 
 export class PuzzleCsv {
   lastUpdated?: Date;
-  //private db: IndexedDBDatabase;
+  private db: Promise<IDBDatabase>;
 
   constructor() {
     const retrieved = this.getItem("last-updated");
     this.lastUpdated = retrieved ? new Date(retrieved) : undefined;
-    //this.db = new IndexedDBDatabase(prefix);
+    this.db = this.openDb();
+  }
+
+  openDb() {
+    const openReq = indexedDB.open(prefix, 1);
+    return new Promise<IDBDatabase>((resolve) => {
+      openReq.onupgradeneeded = () => {
+        let db = openReq.result;
+        if (!db.objectStoreNames.contains(objectStore)) {
+          db.createObjectStore(objectStore, { keyPath: "id" });
+        }
+      };
+      openReq.onerror = (event: any) =>
+        console.error(`DB open error: ${event}`);
+      openReq.onblocked = function () {
+        // this event shouldn't trigger if we handle onversionchange correctly
+        const blocked =
+          "Database is blocked, close all other tabs of this page";
+        console.error(blocked);
+        alert(blocked);
+      };
+      openReq.onsuccess = function () {
+        let db = openReq.result;
+
+        db.onversionchange = function () {
+          db.close();
+          const outdated = "Database is outdated, please reload the page.";
+          console.error(outdated);
+          alert(outdated);
+        };
+        resolve(db);
+      };
+    });
   }
 
   csvIsRecent() {
